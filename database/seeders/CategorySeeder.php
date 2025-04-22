4f28628efc5ee1b17695b4c6ac12ae5bbf487e04
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2025-04-22 16:55:44 +0200

    Aggiornati seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -17,10 +17,10 @@ class CategorySeeder extends Seeder
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
         // 🔁 Pulisce le tabelle nell’ordine corretto: figli → genitori
-        OrderDetail::truncate();   // prima i dettagli ordine
-        Product::truncate();       // poi i prodotti
-        Subcategory::truncate();   // poi le sottocategorie
-        Category::truncate();      // infine le categorie
+        OrderDetail::truncate();
+        Product::truncate();
+        Subcategory::truncate();
+        Category::truncate();
 
         // ✅ Riabilita i vincoli
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
@@ -28,15 +28,44 @@ class CategorySeeder extends Seeder
         // 📦 Categorie principali
         $elettronica = Category::create(['name' => 'Elettronica']);
         $elettrodomestici = Category::create(['name' => 'Elettrodomestici']);
+        $ped = Category::create(['name' => 'Ped']);
 
         // 📂 Sottocategorie per Elettronica
         Subcategory::create(['name' => 'Smartphone', 'slug' => 'smartphone', 'category_id' => $elettronica->id]);
         Subcategory::create(['name' => 'Tablet', 'slug' => 'tablet', 'category_id' => $elettronica->id]);
         Subcategory::create(['name' => 'Computer', 'slug' => 'computer', 'category_id' => $elettronica->id]);
+        Subcategory::create(['name' => 'Televisori', 'slug' => 'televisori', 'category_id' => $elettronica->id]);
+        Subcategory::create(['name' => 'Cuffie e auricolari', 'slug' => 'cuffie-e-auricolari', 'category_id' => $elettronica->id]);
+        Subcategory::create(['name' => 'Smartwatch', 'slug' => 'smartwatch', 'category_id' => $elettronica->id]);
+        Subcategory::create(['name' => 'Console per videogiochi', 'slug' => 'console-per-videogiochi', 'category_id' => $elettronica->id]);
+        Subcategory::create(['name' => 'Macchine fotografiche', 'slug' => 'macchine-fotografiche', 'category_id' => $elettronica->id]);
+        Subcategory::create(['name' => 'Videocamere', 'slug' => 'videocamere', 'category_id' => $elettronica->id]);
+        Subcategory::create(['name' => 'Accessori per PC', 'slug' => 'accessori-per-pc', 'category_id' => $elettronica->id]);
+
+
 
         // 📂 Sottocategorie per Elettrodomestici
         Subcategory::create(['name' => 'Frigoriferi', 'slug' => 'frigoriferi', 'category_id' => $elettrodomestici->id]);
         Subcategory::create(['name' => 'Lavastoviglie', 'slug' => 'lavastoviglie', 'category_id' => $elettrodomestici->id]);
         Subcategory::create(['name' => 'Forni', 'slug' => 'forni', 'category_id' => $elettrodomestici->id]);
+        Subcategory::create(['name' => 'Lavatrici', 'slug' => 'lavatrici', 'category_id' => $elettrodomestici->id]);
+        Subcategory::create(['name' => 'Asciugatrici', 'slug' => 'asciugatrici', 'category_id' => $elettrodomestici->id]);
+        Subcategory::create(['name' => 'Congelatori', 'slug' => 'congelatori', 'category_id' => $elettrodomestici->id]);
+        Subcategory::create(['name' => 'Cappe da cucina', 'slug' => 'cappe-da-cucina', 'category_id' => $elettrodomestici->id]);
+        Subcategory::create(['name' => 'Piani cottura', 'slug' => 'piani-cottura', 'category_id' => $elettrodomestici->id]);
+
+
+        // 📂 Sottocategorie per Piccoli Elettrodomestici (PED)
+        Subcategory::create(['name' => 'Ferri da stiro', 'slug' => 'ferri-da-stiro', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Aspirapolvere', 'slug' => 'aspirapolvere', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Microonde', 'slug' => 'microonde', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Frullatori', 'slug' => 'frullatori', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Macchine da caffè', 'slug' => 'macchine-caffe', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Tostapane', 'slug' => 'tostapane', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Centrifughe', 'slug' => 'centrifughe', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Spremiagrumi', 'slug' => 'spremiagrumi', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Grattugie elettriche', 'slug' => 'grattugie-elettriche', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Macchine per il pane', 'slug' => 'macchine-pane', 'category_id' => $ped->id]);
+        Subcategory::create(['name' => 'Robot da cucina', 'slug' => 'robot-da-cucina', 'category_id' => $ped->id]);
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -17,6 +17,10 @@ class ProductSeeder extends Seeder
         $frigoriferoCategory = Subcategory::where('slug', 'frigoriferi')->first();
         $lavastoviglieCategory = Subcategory::where('slug', 'lavastoviglie')->first();
         $fornoCategory = Subcategory::where('slug', 'forni')->first();
+        $aspirapolvereCategory = Subcategory::where('slug', 'aspirapolvere')->first();
+        $microondeCategory = Subcategory::where('slug', 'microonde')->first();
+        $ferriDaStiroCategory = Subcategory::where('slug', 'ferri-da-stiro')->first();
+        $frigoriferiCategory = Subcategory::where('slug', 'frigoriferi')->first();
 
         // Prodotti per Smartphone
         $this->createProduct([
@@ -39,6 +43,57 @@ class ProductSeeder extends Seeder
             'subcategory_id' => $smartphoneCategory->id,
         ]);
 
+        $this->createProduct([
+            'name' => 'XIAOMI Redmi a3 star blue 4gb ram 128gb rom',
+            'description' => 'Smartphone con display LCD da 6.71", 128 GB di memoria RAM, Bluetooth 5.4, 4G-LTE e supporto Dual SIM. Sistema operativo Android. Fotocamera posteriore da 8 MP e fotocamera frontale da 5 MP. Processore octa-core da 2.2 GHz e batteria da 5000 mAh. Colore Star Blue.',
+            'price' => 156.90,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP849398.jpg?d=800x800',
+            'available_quantity' => 50,
+            'brand' => 'Xiaomi',
+            'subcategory_id' => $smartphoneCategory->id,
+        ]);
+
+        $this->createProduct([
+            'name' => 'Samsung Galaxy Z Flip 5 256GB',
+            'description' => 'Smartphone pieghevole con display Dynamic AMOLED 2X e 256GB di memoria.',
+            'price' => 1105.90,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP861441.jpg?d=400x400',
+            'available_quantity' => 20,
+            'brand' => 'Samsung',
+            'subcategory_id' => $smartphoneCategory->id,
+        ]);
+
+        $this->createProduct([
+            'name' => 'VIVO V21 128GB',
+            'description' => 'Smartphone con display AMOLED da 6.44", 128 GB di memoria interna e 8 GB di RAM. Connettività Bluetooth 5.1, 4G-LTE e 5G-LTE. Supporto Dual SIM. Sistema operativo Android 11. Fotocamera posteriore da 64 MP e frontale da 44 MP. Processore octa-core da 2.4 GHz. Colore Dusk Blue.',
+            'price' => 649.00,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP747152.jpg?d=400x400',
+            'available_quantity' => 30,
+            'brand' => 'Vivo',
+            'subcategory_id' => $smartphoneCategory->id,
+        ]);
+
+        $this->createProduct([
+            'name' => 'OPPO Find x5 lite',
+            'description' => 'Smartphone con display AMOLED da 6.43", 256 GB di memoria interna e 8 GB di RAM. Connettività Bluetooth 5.2, 4G-LTE e 5G-LTE. Supporto Dual SIM. Sistema operativo Android 11 con interfaccia ColorOS 12.1. Fotocamera posteriore da 64 MP. Processore octa-core da 2.4 GHz. Colore Startrails Blue.',
+            'price' => 359.90,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP779098.jpg?d=400x400',
+            'available_quantity' => 40,
+            'brand' => 'Oppo',
+            'subcategory_id' => $smartphoneCategory->id,
+        ]);
+
+        $this->createProduct([
+            'name' => 'Oppo Find X5 Pro 5G 256GB',
+            'description' => 'Smartphone con fotocamera principale da 50MP e display AMOLED da 6.7 pollici.',
+            'price' => 1099.00,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP825516.jpg?d=400x400',
+            'available_quantity' => 25,
+            'brand' => 'Oppo',
+            'subcategory_id' => $smartphoneCategory->id,
+        ]);
+
+
         // Prodotti per Tablet
         $this->createProduct([
             'name' => 'APPLE IPAD PRO 11 WI-FI 256GB STANDARD GLASS - NERO SIDERALE',
@@ -60,6 +115,37 @@ class ProductSeeder extends Seeder
             'subcategory_id' => $tabletCategory->id,
         ]);
 
+        $this->createProduct([
+            'name' => 'APPLE IPAD AIR 5',
+            'description' => 'Tablet con display Retina da 10.9 pollici e 64GB di memoria.',
+            'price' => 599.00,
+            'image' => 'https://example.com/image/ipad_air_5.jpg',
+            'available_quantity' => 30,
+            'brand' => 'Apple',
+            'subcategory_id' => $tabletCategory->id,
+        ]);
+
+        $this->createProduct([
+            'name' => 'LENOVO Tab m11 - tb330fu + tab pen',
+            'description' => 'Tablet con schermo da 10.95", 8 GB di RAM e 128 GB di memoria interna. Processore MediaTek Helio G88 e Bluetooth 5.1. Peso netto di 0.465 Kg. Connettività Wi-Fi 5 (802.11ac). Modulo G non presente.',
+            'price' => 185.90,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP849405.jpg?d=400x400',
+            'available_quantity' => 40,
+            'brand' => 'Lenovo',
+            'subcategory_id' => $tabletCategory->id,
+        ]);
+
+        $this->createProduct([
+            'name' => 'SAMSUNG GALAXY TAB S7 FE',
+            'description' => 'Tablet con display TFT da 12.4 pollici e 64GB di memoria.',
+            'price' => 556.90,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP884574.jpg?d=400x400',
+            'available_quantity' => 20,
+            'brand' => 'Samsung',
+            'subcategory_id' => $tabletCategory->id,
+        ]);
+
+
         // Prodotti per Computer
         $this->createProduct([
             'name' => 'HP Omen 16-wd0006nl 16.1", intel core i7, 32gb ram, 1tb ssd, nvidia geforce rtx 4060, fhd',
@@ -80,6 +166,37 @@ class ProductSeeder extends Seeder
             'brand' => 'Apple',
             'subcategory_id' => $computerCategory->id,
         ]);
+
+        $this->createProduct([
+            'name' => 'ASUS Notebook vivobook display 16" con processore intel®core™ i7-1255u e 16gb ram',
+            'description' => 'Notebook con schermo 13.4", processore Intel Core i7, 16GB di RAM, 512GB SSD, colore argento',
+            'price' => 799.99,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP807790.jpg?d=400x400g',
+            'available_quantity' => 15,
+            'brand' => 'Asus',
+            'subcategory_id' => $computerCategory->id,
+        ]);
+
+        $this->createProduct([
+            'name' => 'HP Victus 15-fa1030nl gaming notebook',
+            'description' => 'Notebook con schermo da 15.6", processore Intel Core i7-13700H a 14 core e 20 thread con velocità di 2.4 GHz. 16 GB di RAM e 512 GB di SSD PCI Express. Connettività Ethernet 10/100/1000 e porta HDMI standard. Colore Mica Silver.',
+            'price' => 1149.90,
+            'image' => 'https://d3s2y7lmzr67yx.cloudfront.net/IMG/EXPERT/PRODUCTS/HD/EXP842925.jpg?d=400x400',
+            'available_quantity' => 12,
+            'brand' => 'HP',
+            'subcategory_id' => $computerCategory->id,
+        ]);
+
+        $this->createProduct([
+            'name' => 'ACER Notebook aspire 3 a315-59-523q - 15.6 pollici - silver',
+            'description' => 'Processore a 1.3 GHz, 16 GB di RAM, 512 GB SSD PCIe. Connessione Ethernet 10/100/1000 e HDMI. Colore silver. Perfetto per prestazioni elevate e design elegante.',
+            'price' => 499.90,
+            'image' => 'https://example.com/image/lenovo_thinkpad_x1_carbon.jpg',
+            'available_quantity' => 8,
+            'brand' => 'Acer',
+            'subcategory_id' => $computerCategory->id,
+        ]);
+
 
         // Prodotti per Frigoriferi
         $this->createProduct([
